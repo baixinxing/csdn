ff1c24861270ac54e80052bd0818b77487857664
baixinxing@163.com <baixinxing@163.com>
2015-05-10 03:10:36 +0800

    做个获取csdn数据之后转到wordpres的插件，顺便改下变成刷csdn阅读量的脚本

--- csdn_view.php
+++ csdn_view.php
@@ -1,36 +1,30 @@
 <?php
 include './vendor/autoload.php';
 
-$client = new Goutte\Client();
+$user_name = 'starparker';
+$times = 2;
 
-//$crawler = $client->request('GET', 'http://blog.csdn.net/StarParker/article/list/1');
-$crawler = $client->request('GET', 'http://blog.csdn.net/StarParker');
+
+$client = new Goutte\Client();
+$crawler = $client->request('GET', 'http://blog.csdn.net/' . $user_name );
 
 $page_count = array_pop(explode('/',$crawler->filter('#papelist > a')->last()->attr('href')));
 
 
 $urls = [];
 for($index=1 ; $index<=$page_count ; $index++){
-	$crawler = $client->request('GET', 'http://blog.csdn.net/StarParker/article/list/' . $index);
+	$crawler = $client->request('GET', 'http://blog.csdn.net/' . $user_name . '/article/list/' . $index);
 	$urls = array_merge($urls, $crawler->filter('#article_list')->filter('.link_title > a')->extract('href'));
 }
 
-$list = [];
-foreach($urls as $key => $url){
-	$dom = $crawler = $client->request('GET', 'http://blog.csdn.net/' . $url);
-	$list[$key]['title']=trim($dom->filter('.link_title > a')->text());
-	try{
-		$list[$key]['category']=$dom->filter('.link_categories > a')->text();
-	}catch(Exception $e){
-		$list[$key]['category']=null;
+for($i=1 ; $i <= $times ; $i++){
+	foreach($urls as $key => $url){
+		$client->request('GET', 'http://blog.csdn.net/' . $url);
+		echo "阅读第[$i]次[($key+1)]篇<br/>\n";
 	}
-	$list[$key]['postdate']=$dom->filter('.link_postdate')->text();
-	$list[$key]['view']=$dom->filter('.link_view')->text();
-	$list[$key]['content']=$dom->filter('#article_content')->html();
-
-echo $list[$key]['title']."\n";
 }
+die("阅读量已刷完!<br/>\n");
 
-die(var_dump($list));
 
+//其实 ab -n 1000 -c 100 $url 更快
 ?>
